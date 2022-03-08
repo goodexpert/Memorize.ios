802ec5e78d7824c1ab2a7d75accd333d82afed36
Steve Park <seongwuk.park@gmail.com>
2022-03-09 11:43:57 +1300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Seongwuk Park on 9/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
