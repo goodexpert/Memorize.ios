a99bbba9e89b998dfbcd685d7221d00e5bedfa65
Steve Park <seongwuk.park@gmail.com>
2022-03-09 14:02:25 +1300

    Lecture 3: MVVM and the Swift type system

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,25 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Seongwuk Park on 9/03/22.
+//
+
+import Foundation
+
+class EmojiMemoryGame {
+    
+    static let emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🛵", "🚠", "🚗", "🚚", "🚇", "🛻", "🚝"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
